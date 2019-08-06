f720e2f1676d5f62baf13a419c4739d69f178569
兼城賢人 <>
2019-08-07 00:30:10 +0900

    productDetail.htm とjs,cssファイル　追加l

---
+++ js/vendor/footer.js
@@ -0,0 +1,8 @@
+
+// フッターを下部に固定
+$(function () {
+  var $ftr = $('#footer');
+  if (window.innerHeight > $ftr.offset().top + $ftr.outerHeight()) {
+    $ftr.attr({ 'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) + 'px;' });
+  }
+});
